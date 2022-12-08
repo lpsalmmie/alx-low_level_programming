781b4db1a307924c1a65fce4dcf2d02802e81ddb
“lpsalmmie” <pittars0001@gmail.com>
2022-12-08 13:45:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ *
+ * Retutn: Always 0 (Success)
+ */
+
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
