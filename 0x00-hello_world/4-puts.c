f3322eb07dc3b3d5322829f5ab9cf82df7eddf94
“lpsalmmie” <pittars0001@gmail.com>
2022-12-08 15:42:23 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * Main - Entry Point
- *
- * Return: Always 0 (Success)
- * /
- 
+ * main - Entry
+ * Return: Always 0
+ * */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
