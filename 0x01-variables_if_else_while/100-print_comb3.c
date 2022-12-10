4bf789da85637971602f014963ce7d4ebabf4ca1
“lpsalmmie” <pittars0001@gmail.com>
2022-12-09 16:37:59 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0' ; tens <= '9'; tens++);/* prints tens digit*/
+	for (tens = '0' ; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
