81c7c23647591e25f0eadba456db886f9de3d7e7
“lpsalmmie” <pittars0001@gmail.com>
2022-12-13 14:57:23 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabrt(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int _print_last_digit(int);
+void jack_bauer(void);
+void tomes_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
