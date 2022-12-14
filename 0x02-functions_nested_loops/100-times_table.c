6cdc247ca76336a703f43126c7c826ba5fc3fcbb
“lpsalmmie” <pittars0001@gmail.com>
2022-12-14 13:29:03 -0800

    Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) = '0');
+					_putchar((k % 10) + '0');
 					}
 			}
 			_putchar('\n');
