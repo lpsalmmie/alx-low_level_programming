526663375c1d62f1883b0859962b0cc32e22bd6d
“lpsalmmie” <pittars0001@gmail.com>
2022-12-13 18:56:04 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,40 +7,24 @@
 
 void jack_bauer(void)
 {
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
+	int i, j;
 
-h2 = 0;
-while (h2 <= 2)
-{
-	if (h2 == 2)
+	i = 0;
+
+
+	while (j < 24)
 	{
-		/*Restrain to 23h, not 29*/
-		a = 3;
-	}
-	h1 + 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		j = 0;
+		while (j < 60)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(',');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(',');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
-		h1++;
+		i++;
 	}
-	h2++;
 }
