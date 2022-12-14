ef28e076deb3dbd3e2f8fdcf2e5c7298530e9be3
“lpsalmmie” <pittars0001@gmail.com>
2022-12-13 20:13:09 -0800

    Battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,14 +18,14 @@ void print_to_98(int n)
 			if (i != 98)
 				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n",i);
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%d, ",j);
+				printf("%d, ", j);
 			else if (j == 98)
 				printf("%d\n", j);
 		}
