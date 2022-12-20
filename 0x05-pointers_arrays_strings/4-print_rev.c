b40589df26a3a01e52181bd407683fd53ad23062
“lpsalmmie” <pittars0001@gmail.com>
2022-12-20 12:51:43 -0800

    Print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
 * @s: the string to be printed in rev
 */
 
-void print_rev(char *s)
+void print_rev(char *s);
 {
 	int i, n;
 
@@ -15,7 +15,7 @@ void print_rev(char *s)
 
 	for (i = n - 1; i >= 0; i--)
 	{
-		putchar(s[i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
