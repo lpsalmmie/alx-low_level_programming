73797d6c8372e73698173c4057ed04656034adfc
“lpsalmmie” <pittars0001@gmail.com>
2023-01-03 13:46:54 -0800

    hot fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * set_string - Entry point
- * @s: input
- * @to: input
- * Return: Always 0 (Success)
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ * Return: void
  */
 
 void set_string(char **s, char *to)
