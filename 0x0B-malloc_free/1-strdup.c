6c90d0f0e2cdb887a8a2a6893806beb1b33bb153
“lpsalmmie” <pittars0001@gmail.com>
2023-01-10 01:28:35 -0800

    Strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	for (end = 0; end <= *str; end++)
 	{
 	}
+
 	end += 1;
 	array = malloc(sizeof(char) * end);
+
 	for (i = 0; i < end; i++)
 		array[i] = str[i];
+
 	if (array == NULL)
 		return (NULL);
 	return (array);
